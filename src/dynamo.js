80a7540554fab71353b5b53411662c4ec91bf8e9
ZHAO Xudong <zxdong@gmail.com>
2019-07-22 15:41:11 +0800

    init (#1)

---
+++ src/dynamo.js
@@ -0,0 +1,54 @@
+import DynamoModel from './model'
+import dynamoose, { Schema } from 'dynamoose'
+import Sequelize from 'sequelize'
+import get from 'lodash.get'
+
+function typeMapper(type) {
+  switch (type) {
+    case Sequelize.STRING:
+      return String
+    case Sequelize.JSON:
+      return Object
+    case Sequelize.BOOLEAN:
+      return Boolean
+    case Sequelize.INTEGER:
+      return Number
+    default:
+      throw new Error(`do not support type: ${type}`)
+  }
+}
+
+export function seqSchemaToDynamoSchema(seqSchema) {
+  let keys = Object.keys(seqSchema)
+  return keys.reduce((prev, k) => {
+    let v = seqSchema[k]
+    let def = {
+      type: typeMapper(v.type)
+    }
+    if (v.defaultValue) {
+      def.default = v.defaultValue
+    }
+    prev[k] = def
+    return prev
+  }, {})
+}
+
+export default class Dynamo {
+  constructor (...args) {
+    let len = args.length
+    let options = args[len - 1]
+    this.options = options
+  }
+
+  define(name, seqSchema) {
+    let conf = seqSchemaToDynamoSchema(seqSchema)
+    let sc = new Schema(conf, {
+      timestamps: get(this.options, 'define.timestamps')
+    })
+    let model = dynamoose.model(
+      name,
+      sc
+    )
+    return new DynamoModel(model, this.options)
+  }
+}
---
+++ src/model.js
@@ -0,0 +1,78 @@
+
+import { generate } from 'shortid'
+
+export default class DynamoModel {
+  constructor(Model, options) {
+    this.Model = Model
+    this.options = options
+    this.useTimeStamp = options.define && options.define.timestamp
+  }
+
+  create(inst) {
+    if (!inst) {
+      throw new Error('create requires instance object')
+    }
+    if (!inst.id) {
+      inst.id = generate()
+    }
+    let ist = new this.Model(inst)
+    return ist.save()
+  }
+
+  findAll (query) {
+    if (!query) {
+      return this.Model.scan().exec()
+    }
+    if (
+      !query ||
+      !query.where ||
+      !Object.keys(query.where).length
+    ) {
+      throw new Error('findAll/find requires where params')
+    }
+    let q = Object.keys(query.where)
+      .reduce((prev, k) => {
+        prev[k] = {
+          eq: query.where[k]
+        }
+        return prev
+      }, {})
+    return new Promise((resolve, reject) => {
+      this.Model.scan(q, (err, result) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
+    })
+  }
+
+  find(...args) {
+    return this.findAll(...args)
+  }
+
+  findByPk (id) {
+    return new Promise((resolve, reject) => {
+      this.Model.get({ id }, (err, result) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
+    })
+  }
+
+  update(update, query) {
+    return new Promise((resolve, reject) => {
+      this.Model.update(query.where, update, (err, result) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
+    })
+  }
+}
